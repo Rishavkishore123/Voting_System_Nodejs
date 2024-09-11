d0269166e18444c34799dd597f0c36e8fa8a289b
RISHAV KISHORE <rishavkishore732@gmail.com>
2024-09-12 03:12:16 +0530

    updating -in-candidate routes

--- Routes/party.js
+++ Routes/party.js
@@ -1,7 +1,7 @@
 const express= require('express');
 
 const router= express.Router();
-const {jwtAuthMiddleware, generateToken}= require('../jwt');
+const {jwtAuthMiddleware,generateToken}= require('../jwt');
 const User= require('./../DB/Sign_up')
 
 const Candidate= require('./../DB/Candidate');
@@ -10,7 +10,7 @@ const Candidate= require('./../DB/Candidate');
 const checkRole=async(userID)=>{
     try{
         const user= await User.findById({userID})
-        if(user.role==='admin'){
+        if(user.role ==='admin'){
             return true;
         }
     }catch(err){
@@ -34,10 +34,35 @@ router.post("/",jwtAuthMiddleware,async(req,resp)=>{
 
 
     }catch(err){
-        resp.status(400).json({message:"User does not saved"})
+        resp.status(404).json({message:"User does not saved"})
     }
 })
 
+router.put("/:candidateID",jwtAuthMiddleware,async(req,resp)=>{
+    try{
+        if(!(await checkRole(req.user.id))){
+            resp.status(401).json({message:"user does not have a admin role"})
+        }
+
+        const candidateID=req.params.candidateID;
+        const updateData= req.body;
+
+        const result= await Candidate.findByIdAndUpdate(candidateID, updateData,{
+            new:true,
+            runValidators:true
+        });
+
+        if(!result){
+            resp.status(401).json({message:"Internal server error"})
+        }
+
+        console.log("Data saved");
+        resp.status(200).json(result)
+
+    }catch(err){
+        resp.status(404).json({message:"candidate not found"})
+    }
+})
 
 
 module.exports= router;
